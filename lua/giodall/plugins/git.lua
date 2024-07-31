213f4b6f70490efb0add9d5d2d3deaa3ffa52b02
giodall <giovanni.dallava@gadz.org>
2024-07-31 09:33:40 +0200

    feat: git new keymaps

--- lua/giodall/plugins/git.lua
+++ lua/giodall/plugins/git.lua
@@ -5,6 +5,8 @@ return {
 		config = function()
 			vim.keymap.set("n", "<leader>gi", ":Git<CR>", { desc = "[I]nterface" })
 			vim.keymap.set("n", "<leader>gp", ":Git push<CR>", { desc = "[P]ush" })
+			vim.keymap.set("n", "<leader>gl", ":Git pull<CR>", { desc = "[L]ush" })
+			vim.keymap.set("n", "<leader>gr", ":Git rebase -i origin/main<CR>", { desc = "[R]ebase interactive" })
 		end,
 	},
 	{
